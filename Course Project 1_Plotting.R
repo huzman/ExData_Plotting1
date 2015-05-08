9eae5caffa7562b51821c40ed340b0a6a4611930
Huy <huy@tweek.tv>
2015-05-08 17:22:41 +0200

    Update Course Project 1_Plotting.R

--- Course Project 1_Plotting.R
+++ Course Project 1_Plotting.R
@@ -8,6 +8,7 @@ dataset <- read.table("household_power_consumption.txt", na.strings="?", header=
 dataset_sub <- dataset[dataset$Date == "1/2/2007" | dataset$Date == "2/2/2007",]
 
 
+
 #clean up column names
 cols <- c("Date", "Time", "GlobalActivePower", "GlobalReactivePower", "Voltage", "GlobalIntensity", "SubMetering1", "SubMetering2", "SubMetering3")
 
@@ -18,14 +19,18 @@ time <- dataset_sub$Time
 dateTime <- paste(dataset_sub$Date, dataset_sub$Time)
 dataset_sub$DateTime <- strptime(dateTime, "%d/%m/%Y %H:%M:%S")
 
-#plot 1
 
+
+#plot 1
+png(filename="plot1.png", width=480, height=480, units = "px")
 hist(dataset_sub$GlobalActivePower, col = "red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
 #plot 2
+png(filename="plot2.png", width=480, height=480, units = "px")
 plot(dataset_sub$DateTime, dataset_sub$GlobalActivePower, ylab="GlobalActivePower (kilowatts)", xlab="", type="l")
 dev.off()
 #plot 3
+png(filename="plot3.png", width=480, height=480, units = "px")
 plot(dataset_sub$DateTime, dataset_sub$SubMetering1, type="l", ylab="Energy sub metering", xlab="", ylim=c(0,theMax))
 par(new=T)
 plot(dataset_sub$DateTime, dataset_sub$SubMetering2, type="l", ylab="Energy sub metering", xlab="", col="red", ylim=c(0,theMax))
@@ -36,6 +41,7 @@ legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=
 
 dev.off()
 #plot 4
+png(filename="plot4.png", width=480, height=480, units = "px")
 par(mfrow=c(2,2))
 plot(dataset_sub$DateTime, dataset_sub$GlobalActivePower, type="l", ylab="Global Active Power", xlab="")
 plot(dataset_sub$DateTime, dataset_sub$Voltage, type="l", ylab="Voltage", xlab="datetime")
